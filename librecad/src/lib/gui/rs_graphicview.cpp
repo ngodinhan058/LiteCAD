f4ddb29dafcf91cccaa1fcd07db5023b0fd98afc
DinhAnNgo <dinhanngo058@gmail.com>
2026-07-30 21:23:49 +0700

    update

--- librecad/src/lib/gui/rs_graphicview.cpp
+++ librecad/src/lib/gui/rs_graphicview.cpp
@@ -1148,7 +1148,6 @@ void RS_GraphicView::drawEntity(RS_Painter *painter, RS_Entity* e, double& patte
     // test if the entity is in the viewport
     if (!isPrinting() &&
         e->rtti() != RS2::EntityGraphic &&
-        e->rtti() != RS2::EntityLine &&
        (toGuiX(e->getMax().x)<0 || toGuiX(e->getMin().x)>getWidth() ||
         toGuiY(e->getMin().y)<0 || toGuiY(e->getMax().y)>getHeight())) {
         return;
